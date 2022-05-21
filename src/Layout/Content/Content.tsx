c7287da44916e86873fff03a03796c17a42087f8
sunburst <3520279278@qq.com>
2022-05-21 12:52:45 +0800

    feat(layout/content.tsx): 引入路由切换动画库

--- src/Layout/Content/Content.tsx
+++ src/Layout/Content/Content.tsx
@@ -1,15 +1,24 @@
+import { PageTransition } from "@steveeeie/react-page-transition";
 import { Layout } from "antd";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { ReLoginModal } from "../../components/ReLoginModal";
 import { MyTabs } from "./components/tabs";
 import style from "./Content.module.scss";
 export function MyContent() {
   const { Content } = Layout;
+  const location = useLocation();
   return (
     <Content className={style.container}>
       <MyTabs></MyTabs>
       <ReLoginModal></ReLoginModal>
-      <Outlet></Outlet>
+      <PageTransition
+        preset="moveToLeftFromRight"
+        transitionKey={location.pathname}
+        enterAnimation="moveFromLeft"
+        exitAnimation="moveToRight"
+      >
+        <Outlet></Outlet>
+      </PageTransition>
     </Content>
   );
 }
--- src/pages/SalesManage/CustomerManage/index.tsx
+++ src/pages/SalesManage/CustomerManage/index.tsx
@@ -1,3 +1,43 @@
+import { Button, Form, Input } from "antd";
+import { SearchOutlined, RedoOutlined } from "@ant-design/icons";
+import style from "./index.module.scss";
 export default function CustomerManage() {
-  return <div>顾客管理</div>;
+  const [form] = Form.useForm();
+  const onReset = () => {
+    form.resetFields();
+  };
+  const onFinish = (values: any) => {
+    console.log(values, "触发");
+  };
+  return (
+    <div className={style.container}>
+      <Form
+        name="basic"
+        labelAlign="right"
+        autoComplete="off"
+        layout="inline"
+        form={form}
+        onFinish={onFinish}
+      >
+        <Form.Item label="顾客姓名" name="customerName">
+          <Input placeholder="请输入顾客姓名"></Input>
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" icon={<SearchOutlined />} htmlType="submit">
+            搜索
+          </Button>
+        </Form.Item>
+        <Form.Item>
+          <Button
+            type="primary"
+            icon={<RedoOutlined />}
+            htmlType="button"
+            onClick={onReset}
+          >
+            重置
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
 }
